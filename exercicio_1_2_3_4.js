49c816614ffcb208ce334e38e043269f12dc419b
Vinicius Gonçalves de Souza <vsouza@intertox.local>
2023-08-07 16:46:24 -0300

    exercicios variaveis

---
+++ exercicio_1_2_3_4.js
@@ -0,0 +1,38 @@
+//Exercicio 1
+const cidadeNatal = "Sao Paulo";
+console.log(cidadeNatal);
+
+
+//Exercicio 2
+let populacao = 12_000_000_000;
+console.log(populacao)
+
+populacao = 13_000_000_000; 
+console.log(populacao)
+
+//Exercicio 3 e 4
+let x = 8, y = 10;
+resultado = x + y
+let mensagem = "A soma de " + x + " + " + y  + " é igual a " + resultado
+console.log(mensagem)
+console.log(`O resultado da soma com parenteses é ${mensagem}`)
+
+
+resultado = x - y
+mensagem = "A subtração de " + x + " + " + y  + " é igual a " + resultado
+console.log(mensagem)
+console.log(`O resultado da subtração com parenteses é ${mensagem}`)
+
+resultado = x * y
+mensagem = "A multiplicação de " + x + " + " + y  + " é igual a " + resultado
+console.log(mensagem)
+console.log(`O resultado da multiplicação com parenteses é ${mensagem}`)
+
+resultado = x/y
+mensagem = "A divisão de " + x + " + " + y  + " é igual a " + resultado
+console.log(mensagem)
+console.log(`O resultado da dvisão com parenteses é ${mensagem}`)
+
+
+
+
