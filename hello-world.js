863a70dc818ef36aa225603cace4450914636798
Vinicius Gonçalves de Souza <vsouza@intertox.local>
2023-08-07 16:47:46 -0300

    Anotações do curso

--- hello-world.js
+++ hello-world.js
@@ -1,1 +1,47 @@
 console.log("Hello World")
+
+
+// operadores aritimeticos
+
+// Contador 
+console.log("Contadores")
+let x = 12
+x += 3      //x = x + 3
+
+let y = 12
+y -= 3     //y = y + 3
+
+let z = 12
+z /= 3      //z = z + 3
+
+console.log(`O resultados dos contadores são ${x}, ${y}, ${z}`)
+console.log("----------------------------------------------------------")
+
+// Incrementação 
+console.log("Incrementação")
+console.log(`O resultados dos incrementos de x são:`)
+x = 10
+console.log(x)
+x++
+console.log (x)
+x--
+console.log(x)
+
+console.log(`O resultados dos incrementos de y são:`)
+y = 10
+console.log(y)
+y++
+console.log(y)
+y-- 
+console.log(y)
+console.log("----------------------------------------------------------")
+
+
+// Raiz quadrada
+console.log("Raiz quadrada")
+let raiz = Math.sqrt(9)
+console.log(raiz)
+
+console.log("Potencia")
+let potencia = Math.pow(2, 3) //usa-se o numero e o expoente dentro dos parenteses
+console.log(potencia)
